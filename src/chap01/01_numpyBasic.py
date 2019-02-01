f8d6cd3bb7e0728388e8ec2c279df0915a875a86
hoon <hoonest01@gmail.com>
2019-02-01 13:12:38 +0900

    numpy 수정

--- src/chap01/01_numpyBasic.py
+++ src/chap01/01_numpyBasic.py
@@ -89,3 +89,101 @@ print(X[X > 15])                  # [51 55 19]
 
 
 
+########################################
+print('###################################')
+# 배열 or 행렬 계산 - NumPy(Numerical Python : 과학적 계산(선형대수))
+# 1. 배열 생성
+x = np.array([10, 20, 30])
+print(x)                # [10 20 30]
+print(type(x))          # <class 'numpy.ndarray'>
+print(x.shape)          # (3,)
+print(x[0], x[1], x[2]) # 10 20 30
+x[2] = 50
+print(x)                # [10 20 50]
+
+y = np.array([[1, 2, 3],[4, 5, 6]])
+print(y)
+print(y.shape) # (2, 3)
+
+print(y[0][1]) # 2
+print(y[0,1])  # 2
+
+print(y[1][0]) # 4
+print(y[1,0]) # 4
+
+# z = np.array([1, 2, 3],[4, 5, 6]) # TypeError: data type not understood
+
+
+# 2. 산술 연산
+x = np.array([1., 2., 3.]) # np.array([1., 2., 3., 4.]) - error
+y = np.array([4., 5., 6.])
+print(x + y)    # [5. 7. 9.]
+print(x - y)    # [-3. -3. -3.]
+print(x * y)    # [ 4. 10. 18.]
+print(x / y)    # [0.25 0.4  0.5 ]
+
+x = np.array([1., 2., 3.])
+print(x / 2.0) # [0.5 1.  1.5]
+
+
+# 3. N차원 배열
+A = np.array([[1, 2],[3, 4]])
+print(A)
+print(A.shape) # (2, 2)
+print(A.dtype) # int32
+
+B = np.array([[3,1],[1,6]])
+
+print(A + B)
+#[[ 4  2]
+# [ 3 10]]
+
+print(A - B)
+#[[-2  2]
+# [ 3 -2]]
+
+print(A * B)
+#[[ 3  0]
+# [ 0 24]]
+
+print(A / B)
+#[[0.33333333        inf]
+# [       inf 0.66666667]]
+
+print(A * 10)
+#[[10 20]
+# [30 40]]
+
+# 4. 브로드캐스트
+X = np.array([[1, 2], [3, 4]])
+Y = np.array([10,20])
+
+print(X + Y)
+print(X - Y)
+print(X * Y)
+print(X / Y)
+
+"""
+# cf) 주의
+Y = np.array([10,20,30])
+print(X + Y) # error
+print(X - Y)
+print(X * Y)
+print(X / Y)
+"""
+
+# 5. 원소 접근(index)
+X = np.array([[51,55], [14,19], [0,4]])
+print(X.shape)  # (3, 2)
+print(X)        #[[51 55]
+                # [14 19]
+                # [ 0  4]]
+print(X[0])     # [51 55]
+print(X[0][1])  # 55
+
+X = X.flatten()
+print(X)  # [51 55 14 19  0  4]
+print(X[np.array([0,2,4])])   # [51 14  0]
+
+print(X > 15)
+print(X[X > 15])
